556b3757b7f87850b0c83dc0c20969ec3b344525
abdulh70 <abdulazizh70@gmail.com>
2024-10-16 05:00:23 +0300

    added file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+const superbowlWin = (arrz) =>{
+    let x= arrz.find((ele)=> ele.result === 'W')
+    if( x === undefined){
+        return x
+    }
+    return x.year
+    }
